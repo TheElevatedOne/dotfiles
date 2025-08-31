3e1a704514a42b99d8dca592cca403e8400fa2b9
Adam Mlady <admin@elevated.ovh>
2025-09-01 01:56:15 +0200

    Update Setup (WIP)

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+ln -s ./btop/ ~/.config/btop
+ln -s ./cava/ ~/.config/cava
+ln -s ./eww/ ~/.config/eww
+ln -s ./fastfetch/ ~/.config/fastfetch
+ln -s ./hellwal/ ~/.config/hellwal
+ln -s ./hypr/ ~/.config/hypr
+ln -s ./kitty/ ~/.config/kitty
+ln -s ./oh-my-posh/ ~/.config/oh-my-posh
+ln -s ./rofi/ ~/.config/rofi
+ln -s ./swaync/ ~/.config/swaync
+ln -s ./waybar/ ~/.config/waybar
+ln -s ./waypaper/ ~/.config/waypaper
+ln -s ./wlogout/ ~/.config/wlogout
